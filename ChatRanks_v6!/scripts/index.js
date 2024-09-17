c2a3bb316c1819d9d8823f6c1c1463b594f5f106
CTabdul15 <117476430+CTabdul15@users.noreply.github.com>
2024-09-18 00:13:03 +0200

    Update index.js

--- ChatRanks_v6!/scripts/index.js
+++ ChatRanks_v6!/scripts/index.js
@@ -4,75 +4,29 @@ import { world, system } from '@minecraft/server'
 const rank = "§7player"
 let scores = "level"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function getScores(scores,player){try{return world.scoreboard.getObjective(scores).getScore(player.scoreboardIdentity)}catch{return 0}}world.beforeEvents.chatSend.subscribe(data=>{let rang=data.sender.getTags().find(tag=>tag.startsWith("rank-"))?.substring(5)?.replaceAll("-","")??`${rank}`;let rang2=data.sender.getTags().find(tag=>tag.startsWith("rank2-"))?.substring(5)?.replaceAll("-","")??``;let score=getScores(scores,data.sender);if(rang){world.sendMessage(`[${score}] [${rang}§r] §r${data.sender.name} ${rang2}§8:§r ${data.message}`)} if (!data.message.startsWith("!")) return data.cancel = true;});system.runInterval(data=>{for(const player of world.getPlayers()){let score=getScores(scores,player);if(player.getTags().find(tag=>tag.startsWith("rank-"))?.substring(5)?.replaceAll("-","")??`${rank}`){player.nameTag=`§r${score} ${player.getTags().find(tag=>tag.startsWith("rank-"))?.substring(5)?.replaceAll("-","")??`${rank}`} §r${player.name}`}else{player.nameTag=`§r${score} ${rank} §r${player.name}`}}});
+function getScores(scores, player) {
+    try {
+        return world.scoreboard.getObjective(scores).getScore(player.scoreboardIdentity)
+    } catch {
+        return 0
+    }
+}
+world.beforeEvents.chatSend.subscribe(data => {
+    let rang = data.sender.getTags().find(tag => tag.startsWith("rank-"))?.substring(5)?.replaceAll("-", "") ?? `${rank}`;
+    let rang2 = data.sender.getTags().find(tag => tag.startsWith("rank2-"))?.substring(5)?.replaceAll("-", "") ?? ``;
+    let score = getScores(scores, data.sender);
+    if (rang) {
+        world.sendMessage(`[${score}] [${rang}§r] §r${data.sender.name} ${rang2}§8:§r ${data.message}`)
+    }
+    if (!data.message.startsWith("!")) return data.cancel = true;
+});
+system.runInterval(data => {
+    for (const player of world.getPlayers()) {
+        let score = getScores(scores, player);
+        if (player.getTags().find(tag => tag.startsWith("rank-"))?.substring(5)?.replaceAll("-", "") ?? `${rank}`) {
+            player.nameTag = `§r${score} ${player.getTags().find(tag=>tag.startsWith("rank-"))?.substring(5)?.replaceAll("-","")??`${rank}`} §r${player.name}`
+        } else {
+            player.nameTag = `§r${score} ${rank} §r${player.name}`
+        }
+    }
+});
